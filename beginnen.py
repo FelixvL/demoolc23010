b542b87820b98db6588e8119c70fb253d4174bd3
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2023-10-03 16:26:00 +0200

    dit is een commit

---
+++ beginnen.py
@@ -0,0 +1,9 @@
+# Vraag de gebruiker om twee getallen in te voeren
+getal1 = float(input("Voer het eerste getal in: "))
+getal2 = float(input("Voer het tweede getal in: "))
+
+# Voer de optelling uit
+resultaat = getal1 + getal2
+
+# Toon het resultaat
+print("Het resultaat van de optelling is:", resultaat)
